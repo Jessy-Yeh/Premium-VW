8ca385d5a38c7bc9c891ba96bb4a4894de12ed76
Jessy Yeh <jiejun.yeh@gmail.com>
2022-03-04 11:50:41 +0000

    Add lightbox for gallery images

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ const menuItemContainer = document.querySelector(".menu-item-container");
 const menu = document.querySelector(".menu");
 const galleryImages = document.querySelectorAll(".gallery-photos img");
 const galleryFilterSelect = document.querySelector(".image-select");
+const lightbox = document.querySelector(".lightbox");
+const lightboxImg = document.querySelector("#lightbox-img");
+
+lightbox.addEventListener("click", function () {
+  lightbox.classList.add("hide-lightbox");
+});
+
+galleryImages.forEach((image) =>
+  image.addEventListener("click", function () {
+    const clickedImgSrc = image.src;
+    lightboxImg.src = clickedImgSrc;
+    lightbox.classList.remove("hide-lightbox");
+  })
+);
 
 hamburger.addEventListener("click", function () {
   menuItemContainer.classList.toggle("menu-display");
